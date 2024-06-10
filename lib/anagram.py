3e1f2bf6a7a670219633e3447e20d955fc52b892
odongo <saonarawestinc@outlook.com>
2024-06-10 07:35:50 +0300

    completed anagram detector lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,7 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+        self.sorted_word = ''.join(sorted(self.word))
+
+    def match(self, list):
+        return [word for word in list if ''.join(sorted(word.lower())) == self.sorted_word]
